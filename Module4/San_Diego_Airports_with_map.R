7dfeadad16a6ebad7cb83726ba8c0eada1ccaf0e
Filip Rychnavsky <filip.r@email.cz>
2017-02-25 00:38:26 +0100

    visualizing with leaflet library

--- Module4/San_Diego_Airports_with_map.R
+++ Module4/San_Diego_Airports_with_map.R
@@ -1,2 +1,12 @@
-# Dataframes, vizualizing
+# Dataframes, visualizing with leaflet library
+usa_airports <- read.csv("https://raw.githubusercontent.com/jflam/VSBlogPost/master/usa_airports.dat", stringsAsFactors = TRUE)
 library(dplyr)
+san_diego_airports <- usa_airports %>% filter(grepl('America', usa_airports$Region, TRUE), grepl('San Diego', usa_airports$city, TRUE))
+head(san_diego_airports)
+library(leaflet)
+map <-
+    san_diego_airports %>%
+    leaflet() %>%
+    addTiles() %>%
+    addCircles( ~ lon, ~ lat, popup = ~name, radius = 200, color = "blue", opacity = 0.8)
+map
