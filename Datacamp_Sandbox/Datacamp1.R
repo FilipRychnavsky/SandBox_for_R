b48c652479ca364c5af173c425e9f171d8b20887
Filip Rychnavský <filip.r@email.cz>
2019-01-01 17:20:18 +0100

    DateTime

--- Datacamp_Sandbox/Datacamp1.R
+++ Datacamp_Sandbox/Datacamp1.R
@@ -3,3 +3,21 @@ print(myVector)
 install.packages("ggvis")
 library("ggvis")
 search()
+
+# The emails vector has already been defined for you
+emails <- c("john.doe@ivyleague.edu", "education@world.gov", "dalai.lama@peace.org",
+						"invalid.edu", "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
+
+# Use grepl() to match for "edu"
+print(grepl(pattern = "edu", emails))
+
+# Use grep() to match for "edu", save result to hits
+hits <- grep(pattern = "edu", emails)
+
+# Subset emails using hits
+#which(hits)
+
+Sys.Date()
+Sys.time()
+my_date <- as.Date("1971-14-05", format = "%Y-%d-%m")
+print(my_date)
