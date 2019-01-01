bf144ac8a8956fd68c1c9b26ef096ac77b977844
Filip Rychnavský <filip.r@email.cz>
2019-01-01 17:28:31 +0100

    example script date time für today und now

--- Datacamp_Sandbox/Datacamp1.R
+++ Datacamp_Sandbox/Datacamp1.R
@@ -22,3 +22,11 @@ Sys.time()
 my_date <- as.Date("1971-14-05", format = "%Y-%d-%m")
 print(my_date)
 print(my_date + 1)
+# Get the current date: today
+today <- Sys.Date()
+# See what today looks like under the hood
+unclass(today)
+# Get the current time: now
+now <- Sys.time()
+# See what now looks like under the hood
+unclass(now)
