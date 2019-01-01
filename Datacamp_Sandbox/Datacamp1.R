6cc2b51f6014dcbc6ad0d153bb1436185d720387
Filip Rychnavský <filip.r@email.cz>
2019-01-01 18:11:28 +0100

    POSIXct for time and string manipulations

--- Datacamp_Sandbox/Datacamp1.R
+++ Datacamp_Sandbox/Datacamp1.R
@@ -47,3 +47,16 @@ date3 <- as.Date(str3, format = "%d/%B/%Y")
 format(date1, "%A")
 format(date2, "%d")
 format(date3, "%b %Y")
+
+# Definition of character strings representing times
+str1 <- "May 23, '96 hours:23 minutes:01 seconds:45"
+str2 <- "2012-3-12 14:23:08"
+
+# Convert the strings to POSIXct objects: time1, time2
+time1 <- as.POSIXct(str1, format = "%B %d, '%y hours:%H minutes:%M seconds:%S")
+time2 <- as.POSIXct(str2, format = "%Y-%m-%d %H:%M:%S")
+
+
+# Convert times to formatted strings
+format(time1, "%M")
+format(time2, "%I:%M %p")
