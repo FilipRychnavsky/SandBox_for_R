d5f51c31e5d363b337d28498ceb557c7fae075f1
Filip Rychnavský <filip.r@email.cz>
2019-01-26 20:24:35 +0100

    installing and loading tidyverse (installing auskommentiert)

---
+++ Datacamp_Sandbox/Tidyverse_sandbox.R
@@ -0,0 +1,14 @@
+# https://campus.datacamp.com/courses/introduction-to-the-tidyverse/data-wrangling-1?ex=2
+#install.packages("gapminder")
+# Load the gapminder package
+
+library(gapminder)
+
+# Load the dplyr package
+library(dplyr)
+
+# Look at the gapminder dataset
+gapminder
+# Filter for the year 1957, then arrange in descending order of population
+
+gapminder %>% filter(year == 1957) %>% arrange(desc(pop))
