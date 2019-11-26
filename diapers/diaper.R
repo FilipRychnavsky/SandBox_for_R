8b67a3e46c676ea5503e32dead81598548b02d32
Filip Rychnavsky <filip.r@email.cz>
2019-11-26 19:48:29 +0100

    Ermittlung von Defecation Date aus DateTime;grouping nach Defecation date und sumarizing; filtering nach Urin

--- diapers/diaper.R
+++ diapers/diaper.R
@@ -1,10 +1,31 @@
 #diaper weight
+
+library(dplyr)
+library(ggplot2)
+
 dir()
 path <- file.path(".", "SandBox_for_R/diapers", "diaper1.csv")
 path
 #diaperData <- read.csv(path, header = TRUE, tz="", format='%d/%m/%Y %H:%M')
 diaperData <- read.csv(path, header = TRUE)
-diaperData$dateTime <- as.POSIXct(diaperData$datetimeText, format = "%m.%d.%Y %H:%M")
+diaperData$defecationDateTime <- as.POSIXct(diaperData$defecationDatetimeText, format = "%m.%d.%Y %H:%M")
+diaperData$defecationDate <- as.Date(diaperData$defecationDateTime)
 
 str(diaperData)
 head(diaperData)
+
+#Datum - group by datum aus dem Defecation DateTime
+# Urin - Sortierung muss nach group by sein
+diaperData %>%
+  group_by(diaperData$defecationDate) %>%
+  filter(excreta == 'U') %>%
+  summarize(totalWeightPerDay = sum(weight))
+  
+
+
+
+# test, wie ich auf DateTime einschränken kann
+# diaperData %>%
+#   filter(diaperData$defecationDateTime> "2019-10-11 12:00")
+# diaperData %>%
+#     arrange(desc(weight))
