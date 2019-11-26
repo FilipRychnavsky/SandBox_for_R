cabc9851184bebe5ca8de29c4c06671b56c8ab9c
Filip Rychnavsky <filip.r@email.cz>
2019-11-26 19:19:13 +0100

    converting works

--- diapers/diaper.R
+++ diapers/diaper.R
@@ -2,5 +2,9 @@
 dir()
 path <- file.path(".", "SandBox_for_R/diapers", "diaper1.csv")
 path
-diaperData <- read.csv(path)
+#diaperData <- read.csv(path, header = TRUE, tz="", format='%d/%m/%Y %H:%M')
+diaperData <- read.csv(path, header = TRUE)
+diaperData$dateTime <- as.POSIXct(diaperData$datetimeText, format = "%m.%d.%Y %H:%M")
+
 str(diaperData)
+head(diaperData)
